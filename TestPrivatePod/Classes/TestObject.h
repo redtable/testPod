103cfc84efe7fc82a4402991de6ce9361b65e354
wangsen <wangsen8@xdf.cn>
2019-08-15 13:32:31 +0800

    first commit

---
+++ TestPrivatePod/Classes/TestObject.h
@@ -0,0 +1,18 @@
+//
+//  TestObject.h
+//  Pods-TestPrivatePod_Example
+//
+//  Created by WangSen on 2019/8/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObject : NSObject
+
++ (void)testLog;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestPrivatePod/Classes/TestObject.m
@@ -0,0 +1,16 @@
+//
+//  TestObject.m
+//  Pods-TestPrivatePod_Example
+//
+//  Created by WangSen on 2019/8/15.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
++ (void)testLog {
+    NSLog(@">>>>>> test private pod log >>>>>");
+}
+
+@end
